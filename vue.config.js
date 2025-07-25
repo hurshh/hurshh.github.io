3b33e3de5a43cd4c8b2a8b913800338b9360a12e
Harsh Rao <hurshrao@gmail.com>
2025-07-25 03:44:40 -0700

    Fix public path and rebuild

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "https://hurshh.github.io/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
 };
